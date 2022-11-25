ea3d457f1dc770aa0c30cc67704044c1d8781755
James Lee <jamesleex86@gmail.com>
2022-11-25 06:05:34 -0800

    Minor changes to CSS

--- react-app/src/components/AllRestaurants/AllRestaurants.js
+++ react-app/src/components/AllRestaurants/AllRestaurants.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getAllRestaurants } from '../../store/restaurants'
 import RestaurantCard from '../RestaurantCard/RestaurantCard'
+import getCurrentLocation from '../../images/get-current-location.png'
 import './AllRestaurants.css'
 
 
@@ -15,11 +16,17 @@ function DisplayAllRestaurants() {
     const allRestaurants = useSelector(state => Object.values(state.restaurants));
 
     return (
-        <div className="card-wrapper">
-            <div className="restaurant-cards">
-                {allRestaurants.map(restaurant => (
-                    <RestaurantCard key={restaurant.id} restaurant={restaurant} />
-                ))}
+        <div className="all-restaurants-container">
+            <div className="above-restaurant-cards">
+                <div className="verify-city">It looks like you're in West Hollywood. Not correct? </div>
+                <div><img src={getCurrentLocation} className="get-current-location" /></div>
+            </div>
+            <div className="card-wrapper">
+                <div className="restaurant-cards">
+                    {allRestaurants.map(restaurant => (
+                        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+                    ))}
+                </div>
             </div>
         </div>
     )
