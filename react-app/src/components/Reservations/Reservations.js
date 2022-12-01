48b208e2a6ca1359d0bfeb1cdf2d7d1e5fb56649
James Lee <jamesleex86@gmail.com>
2022-12-01 11:40:24 -0800

    CSS for reservation component

--- react-app/src/components/Reservations/Reservations.js
+++ react-app/src/components/Reservations/Reservations.js
@@ -2,14 +2,12 @@ import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import { createReservation } from '../../store/reservations';
-import './UpcomingReservationsMenu.css';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
+import './Reservations.css';
+// import './UpcomingReservationsMenu.css';
 
 dayjs.extend(utc);
-
-
-
 
 
 function Reservations() {
@@ -20,7 +18,7 @@ function Reservations() {
 
     const [date, setDate] = useState('');
     const [time, setTime] = useState('');
-    const [partySize, setPartySize] = useState(1);
+    const [partySize, setPartySize] = useState(2);
     const [validationErrors, setValidationErrors] = useState([]);
 
     const submitHandler = async (e) => {
@@ -49,60 +47,71 @@ function Reservations() {
     }
 
 
-
     return (
         <div className="reservation-form-container">
             <form onSubmit={submitHandler}>
-                <h2 className="reservation-form-heading">MAKE A RESERVATION</h2>
-                <div className="reservation-errors-container">
-                    {validationErrors.length > 0 &&
-                        validationErrors.map(error =>
-                            <li key={error}>{error}</li>
-                        )}
-                </div>
-                <div>
-                    <label>
-                        <select value={partySize} onChange={e => setPartySize(e.target.value)}>
-                            <option value="1">1 Person</option>
-                            <option value="2">2 People</option>
-                            <option value="3">3 People</option>
-                            <option value="4">4 People</option>
-                            <option value="5">5 People</option>
-                            <option value="6">6 People</option>
-                            <option value="7">7 People</option>
-                            <option value="8">8 People</option>
-                            <option value="9">9 People</option>
-                            <option value="10">10 People</option>
-                            <option value="11">11 People</option>
-                            <option value="12">12 People</option>
-                        </select>
-                    </label>
-                </div>
-                <div>
-                    <input
-                        type="date"
-                        onChange={e => setDate(e.target.value)}
-                        value={date}
-                        placeholder="Date"
-                    />
-                </div>
-                <div>
-                    <input
-                        type="time"
-                        onChange={e => setTime(e.target.value)}
-                        value={time}
-                        step="1800"
-                        min="17:00"
-                        max="22:00"
-                        placeholder="Time" />
-                </div>
-                <div>
-                    <button
-                        type="submit"
-                        className=""
-                    >
-                        Find a time
-                    </button>
+                <div className="reservation-form">
+                    <div className="reservation-form-heading">
+                        <div className="reservation-heading-font">Make a reservation</div>
+                    </div>
+                    <div className="reservation-errors-container">
+                        {validationErrors.length > 0 &&
+                            validationErrors.map(error =>
+                                <li key={error}>{error}</li>
+                            )}
+                    </div>
+                    <div className="reservation-party-size-label">
+                        <div className="reservation-party-size-text">Party Size</div>
+                        <div className="reservation-party-size-border-bottom">
+                            <label className="reservation-party-size-select">
+                                <select value={partySize} onChange={e => setPartySize(e.target.value)} className="reservation-party-size-select">
+                                    <option value="1">1 Person</option>
+                                    <option value="2">2 People</option>
+                                    <option value="3">3 People</option>
+                                    <option value="4">4 People</option>
+                                    <option value="5">5 People</option>
+                                    <option value="6">6 People</option>
+                                    <option value="7">7 People</option>
+                                    <option value="8">8 People</option>
+                                    <option value="9">9 People</option>
+                                    <option value="10">10 People</option>
+                                    <option value="11">11 People</option>
+                                    <option value="12">12 People</option>
+                                </select>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="reservation-date-time-inputs">
+                        <div className="reservation-date-input-border">
+                            <div className="reservation-date-text">Date</div>
+                            <input
+                                type="date"
+                                onChange={e => setDate(e.target.value)}
+                                value={date}
+                                placeholder="Date"
+                                className="reservation-date-input"/>
+                        </div>
+                        <div className="reservation-time-input-border">
+                            <div className="reservation-time-text">Time</div>
+                            <input
+                                type="time"
+                                onChange={e => setTime(e.target.value)}
+                                value={time}
+                                step="1800"
+                                min="17:00"
+                                max="22:00"
+                                placeholder="Time" 
+                                className="reservation-time-input"/>
+                        </div>
+                    </div>
+                    <div className="reservation-find-time-button-container">
+                        <button
+                            type="submit"
+                            className="reservation-find-time-button"
+                        >
+                            Find a time
+                        </button>
+                    </div>
                 </div>
             </form>
         </div>
