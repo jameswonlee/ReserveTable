09bc619778364d7e1bd7961e4e3e8843091e60a3
James Lee <jamesleex86@gmail.com>
2022-12-02 08:03:28 -0800

    Fixed minor spacing issue on my dining history component

--- react-app/src/store/csrf.js
+++ react-app/src/store/csrf.js
@@ -16,20 +16,15 @@ export async function csrfFetch(url, options = {}) {
     options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
   }
   // call the default window's fetch with the url and the options passed in
-  try {
-    const res = await window.fetch(url, options);
-  
-    // if the response status code is 400 or above, then throw an error with the
-      // error being the response
-    if (res.status >= 400) throw res;
-  
-    // if the response status code is under 400, then return the response to the
-      // next promise chain
-    return res;
+  const res = await window.fetch(url, options);
 
-  } catch (e) {
+  // if the response status code is 400 or above, then throw an error with the
+    // error being the response
+  if (res.status >= 400) throw res;
 
-  }
+  // if the response status code is under 400, then return the response to the
+    // next promise chain
+  return res;
 }
 // call this to get the "XSRF-TOKEN" cookie, should only be used in development
 export function restoreCSRF() {
